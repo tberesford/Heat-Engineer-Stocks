f39f54884870e806a7f0496265c75886c50c5620
Tom Beresford <tberesfords@outook.com>
2025-01-14 15:22:24 +0000

    More tests

--- heat-engineer-client/src/app/services/CalculateShares.ts
+++ heat-engineer-client/src/app/services/CalculateShares.ts
@@ -4,8 +4,15 @@ const CalculateShares = (numberOfShares: number, price: number): number => {
     return numberOfShares * price;
 }
 
-const StockCalculator = (ownedShares: number, currPrice: number, newShares: number, method: string) => {
-    
+const StockCalculator = (ownedShares: number, currPrice: number, shares: number, method: string) => {
+    if(method.toLowerCase() === "sell"){
+        const afterSaleShares = ownedShares - Math.abs(shares);
+        if(afterSaleShares < 0){
+            return "You do not own enough shares"
+        }
+        const saleValue = CalculateShares(shares, currPrice);
+        return [afterSaleShares, saleValue];
+    }
 }
 
 export default StockCalculator;
--- heat-engineer-client/src/app/tests/portfolio.test.tsx
+++ heat-engineer-client/src/app/tests/portfolio.test.tsx
@@ -3,8 +3,23 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import '@testing-library/dom';
 import StockCalculator from "../services/CalculateShares";
 
+interface ISale {
+    ownedShares: number,
+    sharePrice: number,
+    method: string,
+    shares: number
+}
 
 describe("Test User Portfolio", () => {
+    let mockSaleData: ISale;
+    let mockPurchaseData: ISale;
+    let mockErrorData: ISale;
+    beforeAll(() => {
+        mockSaleData = { ownedShares: 0, sharePrice: 5, method: "Sell", shares: -2 };
+        mockPurchaseData = { ownedShares: 0, sharePrice: 5, method: "Buy", shares: 2 };
+        mockErrorData = { ownedShares: 0, sharePrice: 0, method: "Buy", shares: 2 };
+    })
+
     test("button click event", () => {
         const buttonType = "Buy";
         const mockHandler = jest.fn();
@@ -15,10 +30,20 @@ describe("Test User Portfolio", () => {
     });
 
     test("cannot sell shares when owned is zero", () => {
-        const mockData = { ownedShares: 0, sharePrice: 5, method: "Sell", shares: -2 };
-        const result = StockCalculator(mockData.ownedShares, mockData.sharePrice, mockData.shares, mockData.method);
-        expect(result).toEqual("No shares to sell"); 
+        const result = StockCalculator(mockSaleData.ownedShares, mockSaleData.sharePrice, mockSaleData.shares, mockSaleData.method);
+        expect(result).toEqual("You do not own enough shares"); 
     });
-    test.todo("buying shares increases number of owned shares");
-    test.todo("cannot buy if no share price");
+
+    test("buying shares increases number of owned shares", () => {
+        const result = StockCalculator(mockPurchaseData.ownedShares, mockPurchaseData.sharePrice, mockPurchaseData.shares, mockPurchaseData.method);
+        expect(result![0]).toBeGreaterThan(mockPurchaseData.ownedShares);
+    });
+    test("cannot buy if no share price", () => {
+        const result = StockCalculator(mockErrorData.ownedShares, mockErrorData.sharePrice, mockErrorData.shares, mockErrorData.method);
+        expect(result).toEqual("Error purchasing shares - try again later");
+    });
+    test("shares are bought at the correct price", () => {
+        const result = StockCalculator(mockPurchaseData.ownedShares, mockPurchaseData.sharePrice, mockPurchaseData.shares, mockPurchaseData.method);
+        expect(result).toEqual([2, 10]);
+    });
 })
